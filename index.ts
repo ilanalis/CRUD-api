da5d8b3e9c5dd78e924aeac52653138e335a2623
ilana <ilanalipkina@gmail.com>
2025-05-12 12:19:37 +0400

    feat: add error handling for the server

--- index.ts
+++ index.ts
@@ -13,92 +13,125 @@ const server: Server = http.createServer(function (
   req: IncomingMessage,
   res: ServerResponse
 ) {
-  if (req.url === "/api/users" && req.method === RequestMethods.Get) {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(db.getAllUsers()));
-    return;
-  }
-  if (
-    req.url?.match(/^\/api\/users\/[^/]+$/) &&
-    req.method === RequestMethods.Get
-  ) {
-    const id = req.url.split("/")[3];
-    if (!isValidUUID(id)) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Invalid userId format" }));
+  try {
+    if (req.url === "/api/users" && req.method === RequestMethods.Get) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(db.getAllUsers()));
       return;
     }
-
-    const user = db.getUser(id);
-
-    if (!user) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "User doesn't exist" }));
-      return;
-    }
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(user));
-    return;
-  }
-  if (req.url === "/api/users" && req.method === RequestMethods.Post) {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", () => {
-      let bodyData;
-      try {
-        bodyData = JSON.parse(body);
-      } catch {
+    if (
+      req.url?.match(/^\/api\/users\/[^/]+$/) &&
+      req.method === RequestMethods.Get
+    ) {
+      const id = req.url.split("/")[3];
+      if (!isValidUUID(id)) {
         res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Invalid JSON" }));
+        res.end(JSON.stringify({ message: "Invalid userId format" }));
         return;
       }
 
-      const { username, age, hobbies } = bodyData;
-      const requiredFields = ["username", "age", "hobbies"];
+      const user = db.getUser(id);
 
-      const missingFields = requiredFields.filter((field) => !bodyData[field]);
-
-      if (missingFields.length > 0) {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({
-            message: `Missing required fields: ${missingFields.join(", ")}`,
-          })
-        );
+      if (!user) {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "User doesn't exist" }));
         return;
       }
-      const user = db.createUser({ username, age, hobbies });
-      res.writeHead(201, { "Content-Type": "application/json" });
+      res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(user));
-    });
-    return;
-  }
-  if (
-    req.url?.match(/\/api\/users\/\w+/) &&
-    req.method === RequestMethods.Put
-  ) {
-    const id = req.url.split("/")[3];
+      return;
+    }
+    if (req.url === "/api/users" && req.method === RequestMethods.Post) {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
 
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
+      req.on("end", () => {
+        let bodyData;
+        try {
+          bodyData = JSON.parse(body);
+        } catch {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ message: "Invalid JSON" }));
+          return;
+        }
 
-    req.on("end", () => {
-      let bodyData;
-      try {
-        bodyData = JSON.parse(body);
-      } catch {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Invalid JSON" }));
+        const { username, age, hobbies } = bodyData;
+        const requiredFields = ["username", "age", "hobbies"];
+
+        const missingFields = requiredFields.filter(
+          (field) => !bodyData[field]
+        );
+
+        if (missingFields.length > 0) {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(
+            JSON.stringify({
+              message: `Missing required fields: ${missingFields.join(", ")}`,
+            })
+          );
+          return;
+        }
+        const user = db.createUser({ username, age, hobbies });
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(user));
+      });
+      return;
+    }
+    if (
+      req.url?.match(/\/api\/users\/\w+/) &&
+      req.method === RequestMethods.Put
+    ) {
+      const id = req.url.split("/")[3];
+
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        let bodyData;
+        try {
+          bodyData = JSON.parse(body);
+        } catch {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ message: "Invalid JSON" }));
+          return;
+        }
+
+        if (!isValidUUID(id)) {
+          console.log("not valid");
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ message: "Invalid userId format" }));
+          return;
+        }
+        const user = db.getUser(id);
+
+        if (!user) {
+          console.log("user dosnt exist");
+
+          res.writeHead(404, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ message: "User doesn't exist" }));
+          return;
+        }
+
+        const updatedUser = db.updateUser(id, bodyData);
+        if (!updatedUser) return;
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(updatedUser));
         return;
-      }
+      });
+      return;
+    }
+    if (
+      req.url?.match(/\/api\/users\/\w+/) &&
+      req.method === RequestMethods.Delete
+    ) {
+      const id = req.url.split("/")[3];
 
       if (!isValidUUID(id)) {
-        console.log("not valid");
         res.writeHead(400, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "Invalid userId format" }));
         return;
@@ -106,44 +139,21 @@ const server: Server = http.createServer(function (
       const user = db.getUser(id);
 
       if (!user) {
-        console.log("user dosnt exist");
-
         res.writeHead(404, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "User doesn't exist" }));
         return;
       }
 
-      const updatedUser = db.updateUser(id, bodyData);
-      if (!updatedUser) return;
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(updatedUser));
-      return;
-    });
-    return;
-  }
-  if (
-    req.url?.match(/\/api\/users\/\w+/) &&
-    req.method === RequestMethods.Delete
-  ) {
-    const id = req.url.split("/")[3];
-
-    if (!isValidUUID(id)) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Invalid userId format" }));
+      res.writeHead(204, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(db.deleteUser(id)));
       return;
     }
-    const user = db.getUser(id);
-
-    if (!user) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "User doesn't exist" }));
-      return;
-    }
-
-    res.writeHead(204, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(db.deleteUser(id)));
-    return;
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Not found" }));
+  } catch (error) {
+    console.log("error", error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Internal Server Error" }));
   }
 });
 
