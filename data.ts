e1bc8232a4aef948a7712b5f086e9a36a75b0506
ilana <ilanalipkina@gmail.com>
2025-05-12 12:12:12 +0400

    feat: create db, get and post methods

---
+++ data.ts
@@ -0,0 +1,38 @@
+import { generateUUID } from "./utils/generateUUID";
+
+interface User {
+  id: string;
+  username: string;
+  age: number;
+  hobbies: string[];
+}
+
+export class DataBase {
+  private users: { [key: string]: User } = {};
+  getAllUsers() {
+    return this.users;
+  }
+
+  getUser(id: string): User | undefined {
+    return this.users[id];
+  }
+
+  createUser(user: Omit<User, "id">): User {
+    const id = generateUUID();
+    const newUser = { ...user, id };
+    this.users[id] = newUser;
+    return newUser;
+  }
+
+  updateUser(id: string, updates: User): User | undefined {
+    const foundUser = this.users[id];
+    if (!foundUser) return;
+    const updatedUser = { ...foundUser, ...updates };
+    this.users[id] = updatedUser;
+    return updatedUser;
+  }
+  deleteUser(id: string) {
+    delete this.users[id];
+    return this.users;
+  }
+}
---
+++ index.ts
@@ -0,0 +1,59 @@
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+import { DataBase } from "./data";
+import { RequestMethods } from "./types";
+import dotenv from "dotenv";
+import { isValidUUID } from "./utils/isValidUUID";
+dotenv.config();
+
+const port = process.env.PORT;
+
+const db: DataBase = new DataBase();
+
+const server: Server = http.createServer(function (
+  req: IncomingMessage,
+  res: ServerResponse
+) {
+  if (req.url === "/api/users" && req.method === RequestMethods.Get) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(db.getAllUsers()));
+    return;
+  }
+  if (
+    req.url?.match(/^\/api\/users\/[^/]+$/) &&
+    req.method === RequestMethods.Get
+  ) {
+    const id = req.url.split("/")[3];
+    const user = db.getUser(id);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(user));
+    return;
+  }
+  if (req.url === "/api/users" && req.method === RequestMethods.Post) {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      let bodyData;
+      try {
+        bodyData = JSON.parse(body);
+      } catch {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Invalid JSON" }));
+        return;
+      }
+
+      const { username, age, hobbies } = bodyData;
+
+      const user = db.createUser({ username, age, hobbies });
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(user));
+    });
+    return;
+  }
+});
+
+server.listen(port, () => {
+  console.log("Server is listening on port " + port);
+});
